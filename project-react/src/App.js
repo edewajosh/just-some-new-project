e01d677f156fa46e67a8ab0af5a8a2dc77bca095
edewajosh <joshedewa@gmail.com>
2020-07-25 17:26:07 +0300

    React state

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -3,29 +3,17 @@ import Tweet from './tweet';
 
 function App() {
 
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-  const [user, setUser] = useState({
-    name: 'Josh',
-    age: 25,
-    posts: ['my first post', 'my lovely summer']
-  });
-
-  const increment = () => {
-    setCount(count + 1);
-    setRed(!isRed);
-  };
+  const [users, setUsers] = useState([
+    {name: 'Josh',message: 'my first post my lovely summer'},
+    {name: 'John',message: 'This is your lucky day'},
+    {name: 'Jemmie',message: 'It is what is it is'},
+  ]);
 
   return (
     <div className="app">
-      {/* <Tweet name="Jane" message="Learning React with DevEd" />
-      <Tweet name="Doe" message="Learning React-hooks with DevEd" />
-      <Tweet name="Traversy" message="Learning React State Management" />
-      <Tweet name="Smith" message="Software Development soft skills" />
-      <Tweet name="John" message="Learning React with DevEd" /> */}
-      <h1 className={isRed ? 'red': ''}>Change My Color</h1>
-      <button onClick={increment} className='button'>Increment</button>
-    <h1>{count}</h1>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
 
     </div>
   );
